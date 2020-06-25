a7ae6fe9434cf349dc68b07b9e6ba7d7e1c8ab1e
RebornInferno <antonpirogov@mail.ru>
2020-06-25 15:22:06 +0300

    typo

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' })
 
 
-router.post("/fileanalyse",upload.singe('fileToTest'),async (req, res, next) => {
+router.post("/fileanalyse",upload.single('fileToTest'),async (req, res, next) => {
 
   res.json({"name":req.file.originalname,"type":req.file.encoding,"size":req.file.size})
 });
